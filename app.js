ba2b363a5c78a5f219b0f9ef36df1500a259bdd6
Mohammed Mulazada <thisismoniac@gmail.com>
2018-03-22 12:42:06 +0100

    added building data to server

--- app.js
+++ app.js
@@ -1,20 +1,44 @@
 const ejs = require('ejs')
 const express = require('express')
+const fetch = require('node-fetch')
 const path = require('path')
 const app = express()
+let data
+
+function getData() {
+
+	fetch('http://verdwenengebouwen.nl/api/search?place=amsterdam')
+		.then( res => res.json() )
+		.then( res => {
+			data = res.results.sort((a, b) => a.name.localeCompare(b.name))
+		} )
+		.catch( err => console.log( err ) )
+
+}
+
+getData()
 
 app.set('view engine', 'ejs')
 
 app.use('/static', express.static(path.join(__dirname, 'public')))
-console.log(path.join(__dirname, '../public'))
 app.use(express.static('public'))
 
 const port = 3000
 
 app.get('/', (req, res) => {
-	res.render('index')
+	console.log(data)
+	res.render('index', {
+		data
+	})
 })
 
 app.listen(port, () => {
 	console.log(`Server is running at port ${port}`)
 })
+
+// const apiCall = async () => {
+// 	const response = await fetch('http://verdwenengebouwen.nl/gebouw/json/3569')
+// 	const data = await response.json()
+// 	if (data) {console.log('woo')}
+// 	return data
+// }
---
+++ public/scripts/script.js
@@ -0,0 +1,3 @@
+console.log('hossssi')
+
+const data = 'test'
