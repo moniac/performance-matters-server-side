fd0a2c0e93085b77e20dcd95fc15d1caa7b7437f
Mohammed Mulazada <thisismoniac@gmail.com>
2018-03-23 09:30:05 +0100

    start on readme

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # performance-matters-server-side
+
+# Table of Contents
+1. [Setting up](#Setup)
+2. [Tooling](#Tooling)
+3. [Example](#example)
+
+## Setup
+
+To run this project:
+
+``` git clone && npm run start ```
+
+This will get watchify to watch the files & make the project available at http://localhost:3000.
+
+## Tooling
+
+For this project I decided to step away from Webpack and try my hand at NPM scripts.
+
+The workflow exists of 2 main scripts:
+
+### npm start
+
+This will watch the /client folder where the Javascript is, bundle on any change, export to the /public/scripts folder and run nodemon.
+
+``` "start": "npm run watch & nodemon app.js"```
+
+### npm build
+
+This will prepare the javascript code for deployment, minifying the Javascript code using uglify-js.
+
+``` "build": "browserify ./client/script.js | uglifyjs -cm > public/scripts/bundle.js" ```
+
--- public/scripts/bundle.js
+++ public/scripts/bundle.js
@@ -1,4 +1,46 @@
 (function(){function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s}return e})()({1:[function(require,module,exports){
-"use strict";
+'use strict';
 
-},{}]},{},[1]);
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+var newInput = document.createElement('input');
+var container = document.querySelector('section');
+container.prepend(newInput);
+newInput.id = 'myInput';
+newInput.placeholder = 'Zoeken op gebouwen';
+
+var inputs = document.querySelector('#myInput');
+
+inputs.addEventListener('keyup', filterList);
+
+function filterList() {
+	var input = void 0,
+	    filter = void 0,
+	    ul = void 0,
+	    li = void 0,
+	    a = void 0,
+	    i = void 0;
+	input = document.getElementById('myInput');
+	filter = input.value.toLowerCase();
+	ul = document.getElementById("myUL");
+	li = ul.getElementsByTagName('li');
+
+	for (i = 0; i < li.length; i++) {
+		a = li[i].getElementsByTagName("a")[0];
+		if (a.innerHTML.toLowerCase().indexOf(filter) > -1) {
+			li[i].style.display = "";
+		} else {
+			li[i].style.display = "none";
+		}
+	}
+}
+
+exports.filterList = filterList;
+
+},{}],2:[function(require,module,exports){
+'use strict';
+
+var _filter = require('./filter');
+
+},{"./filter":1}]},{},[2]);
